eb70e10682abd01cabba95ad4d1f30e5266f1daf
ns4ever <45993071+ns4ever@users.noreply.github.com>
2018-12-19 19:22:35 +0530

    Add files via upload
    b

---
+++ main.tf
@@ -0,0 +1,53 @@
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = "1bb6eeda-d07d-4234-8c09-51e3c4195fa0"
+  client_id       = "b5572627-5d92-47ff-a6cd-7fbab596938e"
+  client_secret   = "W312SU1gy4Gwdakyp4lRrSuT4yqe8X1o1LouJukdOhQ="
+  tenant_id       = "6132875a-61c9-4549-b266-fe19f482fd8b"
+}
+
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "newsto"
+    container_name       = "testcon"
+    access_key = "YOB5Qg/lqGFD3ypHC7k+lS1ZtEFdmBCsNP/lcpUCFr06890WLXUb8myO5d3fPNMFd0ivAW8K/Wu1OzimLpRZ/Q=="
+    key                  = "terraform.tfstate"
+  }
+}
+
+# create a resource group 
+resource "azurerm_resource_group" "RG" {
+    name = "testrgnew30"
+    location = "West US"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+   name = "teststoragenew30"
+   resource_group_name = "${azurerm_resource_group.RG.name}"
+   location = "${azurerm_resource_group.RG.location}"
+   account_tier = "Standard"
+   account_replication_type = "GRS"
+}
+resource "azurerm_storage_container" "blobstorage" {
+   name = "testcontainernew30"
+   resource_group_name = "${azurerm_resource_group.RG.name}"
+   storage_account_name = "${azurerm_storage_account.storageaccount.name}"
+   container_access_type = "container" /*Can be either blob, container or private. Defaults to private.*/
+}
+resource "random_id" "random" {
+  keepers {
+    tm = "${timestamp()}"
+  }
+  byte_length = 8
+}
+
+resource "azurerm_snapshot" "snapshot" {
+  name                = "snapshot-${random_id.random.hex}"
+  location            = "West US"
+  resource_group_name = "test"
+  create_option       = "Copy"
+ source_uri      ="/subscriptions/1bb6eeda-d07d-4234-8c09-51e3c4195fa0/resourceGroups/test/providers/Microsoft.Storage/storageAccounts/newsto/blobServices/testcon/terraform.tfstate"
+}
+
